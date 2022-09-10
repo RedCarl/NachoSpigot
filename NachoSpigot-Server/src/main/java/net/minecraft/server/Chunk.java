9d7d6faaf4b2db32bc5952b72a8c1c4f81f33038
caoli5288 <caoli5288@gmail.com>
2022-09-11 00:37:27 +0800

    Revert IonSpigot-0014. It's caused server crash

--- NachoSpigot-Server/src/main/java/net/minecraft/server/Chunk.java
+++ NachoSpigot-Server/src/main/java/net/minecraft/server/Chunk.java
@@ -151,7 +151,7 @@ public class Chunk {
         this.heightMap = new int[256];
 
         for (int k = 0; k < this.entitySlices.length; ++k) {
-            this.entitySlices[k] = new io.papermc.paper.util.maplist.ObjectMapList<>(); // IonSpigot - UnsafeList -> ObjectMapList // Spigot
+            this.entitySlices[k] = new org.bukkit.craftbukkit.util.UnsafeList(); // Spigot
         }
 
         Arrays.fill(this.f, -999);
